32a3bdcfa0884c7afba13a75869a66472cdcd5d5
facu <facundo19burgos@gmail.com>
2023-08-29 22:54:26 -0300

    cors y carpeta de helperStatusCode

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+
+app.listen(PORT, () => {
+  console.log(`el server se levanto ${PORT}`)
+})
