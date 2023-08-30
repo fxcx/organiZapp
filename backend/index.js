f640746bffb3dc8c4f1af232623350116b06d701
facu <facundo19burgos@gmail.com>
2023-08-30 00:28:00 -0300

    router getUser

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
+import { userRouter } from './routers/userRouter.js'
 dotenv.config()
 const PORT = process.env.PORT || 3000
-const app = express()
 
+const app = express()
 app.use(cors())
+app.use(express.json())
+
+// router method
+app.use('/api', userRouter())
 
 app.listen(PORT, () => {
   console.log(`el server se levanto ${PORT}`)
---
+++ backend/router/userRouter.js
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import { userController } from '../controllers/'
+
+export const userRouter = () => {
+  const userRouter = Router()
+  // destructuro las funciones que vienen controller, la logica que usaria con prisma
+  const { getUser, getUserById, createUser, updateUser, deleteUser } = userController()
+
+  userRouter.route('/')
+    .get(getUser)
+    .post(createUser)
+
+  userRouter.route('/user/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+  return userRouter
+}
