aa7d112cdbcec59a398122378660875d7bc5d52e
facu <facundo19burgos@gmail.com>
2023-09-09 00:42:45 -0300

    conection/ submits messages

--- frontend/src/app/chat/page.jsx
+++ frontend/src/app/chat/page.jsx
@@ -1,11 +1,62 @@
-import io from 'socket.io-client'
+'use client'
+
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+// const socket = io("http://localhost:3001");
+const socket = io("/");
+
+export default function App() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("message", receiveMessage)
+
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, []);
+
+  const receiveMessage = (message) =>
+    setMessages(state => [message, ...state]);
 
 
-export default function Chat() {
-  const socket = io("/")
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages(state => [newMessage, ...state]);
+    setMessage("");
+    socket.emit("message", newMessage.body);
+  };
+
   return (
-  <>
-  <div>hello</div>
-  </>
-  )
+    <div>
+      <form>
+        <h1>Chat React</h1>
+        <input
+          name="message"
+          type="text"
+          placeholder="Write your message..."
+          onChange={(e) => setMessage(e.target.value)}
+          className="border-2 border-zinc-500 p-2 w-full text-black"
+          value={message}
+          autoFocus
+        />
+
+        <ul>
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"}`}>
+              <b>{message.from}</b>:{message.body}
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
 }
