9bcddc5c9985d59b7bbeb851a6498dc1c826efc8
facu <facundo19burgos@gmail.com>
2023-09-18 00:29:33 -0300

    update depends

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,39 @@
 import express from 'express'
+// import { readFileSync } from 'fs'
+// import https from 'https'
 import http from 'http'
-// import fs from 'fs'
 import { Server } from 'socket.io'
+import cors from 'cors'
 import errorHandler from './middleware/errorHandler.js'
 import { userRouter } from './router/userRouter.js'
 import { taskRouter } from './router/taskRouter.js'
-import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 const PORT = process.env.PORT || 4000
 
-// const options = {
-//   key: fs.readFileSync('./certificates/key.pem'), // Ruta a tu clave privada
-//   cert: fs.readFileSync('./certificates/cert.pem')// Ruta a tu certificado SSL/TLS
-// }
-const corsOptions = {
-  origin: 'http://localhost:3000', // Permitir solicitudes desde tu aplicación Next.js
-  methods: ['GET', 'POS'], // Métodos HTTP permitidos
-  credentials: true
-}
-
-app.use(cors(corsOptions))
+// const server = https.createServer(app, {
+//   key: readFileSync('./certificates/key.pem', 'utf-8'),
+//   cert: readFileSync('./certificates/cert.pem', 'utf-8')
+// })
 
 const server = http.createServer(app)
-const io = new Server(server)
+
+const io = new Server(server, {
+  cors: {
+    origin: 'localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
 
 io.on('connection', (socket) => {
-  console.log('cliente conectado', socket.id)
-
-  socket.on('message', (body) => {
-    // emitir msj a todos
-    socket.broadcast.emit('message', {
-      body,
-      from: socket.id
-    })
-  })
+  console.log('hola estas conectado')
 })
+
+io.emit()
 
 // router
 app.use('/api', userRouter(), taskRouter())
--- frontend/src/sections/chat/chat-view.jsx
+++ frontend/src/sections/chat/chat-view.jsx
@@ -1,24 +1,30 @@
-"use client";
+"use client"
 
 import { useEffect, useState } from "react";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("localhost:4000", {
+  autoConnect:true
+});
 
 export default function ChatView() {
   const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState("");
+  const [newMessage, setNewMessage] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newMessage = {
-      body: message,
+    const newMessageInfo = {
+      body: newMessage,
       from: "Me",
     };
-    setMessages([...messages, newMessage]);
-    socket.emit("message", message)
-    setMessage("")
+    setMessages([...messages, newMessageInfo]);
+    socket.emit("message", newMessage)
+    setNewMessage("")
   };
+
+  const receiveMessage = (message) => {
+    setNewMessages((previousMessages) => [...previousMessages, message]);
+  }
 
   useEffect(() => {
     socket.on("message", receiveMessage);
@@ -29,9 +35,6 @@ export default function ChatView() {
   }, []);
 
 
-  const receiveMessage = (message) => {
-    setMessages((state) => [...state, message]);
-  }
   
   return (
     <div>
@@ -40,9 +43,9 @@ export default function ChatView() {
         <input
           type="text"
           placeholder="Write your message..."
-          onChange={(evento) => setMessage(evento.target.value)}
+          onChange={(evento) => setNewMessages(evento.target.value)}
           className="border-2 border-zinc-500 p-2 w-full text-black"
-          value={message}
+          value={newMessage}
           autoFocus
         />
         <button type="submit">send</button>
