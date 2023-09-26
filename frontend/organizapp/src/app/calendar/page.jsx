e65319e6a0808c8820dbcc2a743ac3a123f995a3
Agustín Gercek <gercekagustin14@gmail.com>
2023-09-26 17:33:50 -0300

    Added calendar

--- frontend/organizapp/src/app/calendar/page.jsx
+++ frontend/organizapp/src/app/calendar/page.jsx
@@ -20,12 +20,16 @@ function Calendario() {
         </div>
       </div>
       <div>
-        <h2 className='text-center mr-32 mt-36'>
+        <h2 className='text-center ml-12 mr-32 mt-36'>
           <span className='text-2xl'>Selected date:</span>{' '}
           {date.toDateString()}
          </h2>
-        <div className="mt-16 mr-32">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ex dignissimos nobis? Maiores dignissimos eius esse accusantium soluta eaque assumenda asperiores quisquam corporis velit, quas, iusto odit, aperiam hic repellendus!
+        <div className="mt-16 ml-12 mr-32">
+        Of is two. Behold beast. Kind tree made. Make dry them days and made dominion said it given.
+
+Firmament heaven. Male cattle light of place male wherein behold. Whose evening man light multiply dry. Divided us blessed be fill kind made firmament, from in shall over behold, that herb. Creature very face land set waters night green.
+
+Place And day their. Beast also lights made don't firmament to is divided shall life good, own there fill lights over you'll there said doesn't third appear which second. Of, hath replenish they're shall fly spirit forth itself. Gathered subdue make the bearing.
         </div>
       </div>
     </div>
